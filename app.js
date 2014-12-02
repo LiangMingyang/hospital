5a79e5d43633e6b46a639fa8d2443eb635bb4aae
明阳 <lmysoar@hotmail.com>
2014-12-02 22:45:38 +0800

    修改了一下网页名称

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.set('title', 'App server');
+app.set('title', 'Database server');
 
 app.use(logger('dev'));
 app.use(cookieParser());
