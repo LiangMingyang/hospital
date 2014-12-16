710f559f93d129ebcb9953c8732356e9204020fb
明阳 <lmysoar@hotmail.com>
2014-12-17 00:55:15 +0800

    fixed bugs

--- my_modules/database/router.js
+++ my_modules/database/router.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var db = require('./database');
 var dbhelper = require('./dbhelper');
 var fs = require('fs');
 var markdown = require('markdown').markdown;
